301c01f1971e09832b641bb2d9ecc9f778df2c17
z.jiryaee <zahrajiryaee77@gmail.com>
2022-08-17 20:01:02 +0430

    SignOut a User

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -43,6 +43,19 @@ const Navbar = () => {
     setShowDropdown(!showDropdown);
   };
 
+  const handleSignout = async (e) => {
+    e.preventDefault();
+
+    try {
+      await magic.user.logout();
+      console.log(await magic.user.isLoggedIn()); // => false
+      router.push("/login");
+    } catch (error) {
+      console.error("Error logging out", error);
+      router.push("/login");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -84,9 +97,9 @@ const Navbar = () => {
             {showDropdown && (
               <div className={styles.navDropdown}>
                 <div>
-                  <Link href="/login">
-                    <a className={styles.linkName}>Sign out</a>
-                  </Link>
+                  <a className={styles.linkName} onClick={handleSignout}>
+                    Sign out
+                  </a>
                   <div className={styles.lineWrapper}></div>
                 </div>
               </div>
