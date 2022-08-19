ef16eaee19b68c7f8d5c42b0503a5f7e3845b373
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2022-08-19 17:31:20 +0430

    Youtube API by ID

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -3,23 +3,42 @@ import { useRouter } from "next/router";
 import Modal from "react-modal";
 import clsx from "classnames";
 
+import { getYoutubeVideoById } from "../../lib/videos";
+
 import styles from "../../styles/Video.module.css";
 
 Modal.setAppElement("#__next");
 
-const Video = () => {
+export async function getStaticProps() {
+  const videoId = "mYfJxlgR2jw";
+  const videoArray = await getYoutubeVideoById(videoId);
+
+  return {
+    props: { video: videoArray.length > 0 ? videoArray[0] : {} },
+    revalidate: 10,
+  };
+}
+
+export async function getStaticPaths() {
+  // Get the paths we want to pre-render based on posts
+  const listOfVideos = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"];
+  const paths = listOfVideos.map((videoId) => ({
+    params: { videoId },
+  }));
+
+  return { paths, fallback: "blocking" };
+}
+
+const Video = ({ video }) => {
   const router = useRouter();
 
-  const video = {
-    title: "title",
-    publishTime: "publishTime",
-    description:
-      "description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description",
-    channelTitle: "channelTitle",
-    viewCount: 1000000,
-  };
-
-  const { title, publishTime, description, channelTitle, viewCount } = video;
+  const {
+    title,
+    publishTime,
+    description,
+    channelTitle,
+    statistics: { viewCount } = { viewCount: 0 },
+  } = video;
 
   return (
     <div className={styles.container}>
@@ -37,7 +56,7 @@ const Video = () => {
           width="100%"
           height="360"
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
-          frameborder="0"
+          frameBorder="0"
         ></iframe>
 
         <div className={styles.modalBody}>
