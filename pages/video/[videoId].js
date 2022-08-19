ae1e96a611b5ebcc2afca2c911f3545103a4d1bb
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2022-08-19 16:17:32 +0430

    modal structure & styling

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 
 import Modal from "react-modal";
+import clsx from "classnames";
 
 import styles from "../../styles/Video.module.css";
 
@@ -8,6 +9,17 @@ Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
+
+  const video = {
+    title: "title",
+    publishTime: "publishTime",
+    description:
+      "description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description",
+    channelTitle: "channelTitle",
+    viewCount: 1000000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
 
   return (
     <div className={styles.container}>
@@ -18,15 +30,35 @@ const Video = () => {
         className={styles.modal}
         overlayClassName={styles.overlay}
       >
-        <h1>Video {router.query.videoId}</h1>
         <iframe
           id="ytplayer"
+          className={styles.videoPlayer}
           type="text/html"
-          width="640"
+          width="100%"
           height="360"
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
           frameborder="0"
         ></iframe>
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
